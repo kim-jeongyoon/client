f46737013db94953c3530635688367e400236d6d
kim-jeongyoon <kjyoon811@gmail.com>
2023-01-03 13:45:21 +0900

    02_commit

---
+++ client/src/edu/kh/network/run/ClientRun.java
@@ -0,0 +1,14 @@
+package edu.kh.network.run;
+
+import edu.kh.network.model.service.TCPClient;
+
+public class ClientRun {
+
+	public static void main(String[] args) {
+		TCPClient service = new TCPClient();
+		service.clientStart();
+		
+
+	}
+
+}
